16d6775497ba3a4b056e10917c7880896a22ab12
Renato Richard <Renatorichard2012@gmail.com>
2019-05-07 14:08:53 -0300

    Modo single player - definindo categoria

--- FOULBOGEY.c
+++ FOULBOGEY.c
@@ -1,54 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #define TAM 50
 /*
-void flush_in() {
-    int ch;
-    do {
-        ch = fgets(stdin);
-    } while (ch != EOF && ch != '\n');
-}
-*/
-int multiPlayers(char playerOne[TAM], char playerTwo[TAM], int lifeOne, int lifeTwo, int lifeCraw){
+int multiPlayers(char playerOne[TAM], char playerTwo[TAM], int lifeOne, int lifeTwo, int lifeCraw, char characterOne[TAM], char characterTwo[TAM]){
 	printf("=========================================================\n");
 	printf("        BEM VINDO AO LEAGUE OF POBRE - Orc Detestavel         \n");
 	printf("=========================================================\n");
-	printf("Player01: %c   Vida: %d \n", playerOne, lifeOne);
-	printf("Player02: %c   Vida: %d \n", playerTwo, lifeTwo);
-	printf("INIMIGO: Crawmerax       Vida: &d \n", lifeCraw);
-	printf("=========================================================\n");
-}
-int singlePlayer(char playerOne[TAM], int lifeOne, int lifeCraw){
-	printf("=========================================================\n");
-	printf("Player01: %c   Vida: %d \n", playerOne, lifeOne);
+	printf("Player01: %c   Vida: %d  Classe: %c \n", playerOne, lifeOne, characterOne);
+	printf("Player02: %c   Vida: %d  Classe: %c \n", playerTwo, lifeTwo, characterTwo);
 	printf("INIMIGO: Crawmerax       Vida: %d \n", lifeCraw);
 	printf("=========================================================\n");
 }
-//int barbaro(int life, int cure, int offense, int meleeatack){}
-int classCharacter(){
-	int classe;
-	printf("Qual sua classe?:  \n");
-	printf("03 - Bar \n");
-	printf("04 - Arqueiro \n");
-	printf("05 - Mago \n");
-	printf("Escolha: ");
-	scanf("%d\n", &classe);
+*/
+int singlePlayer(char playerOne[TAM], int lifeOne, int lifeCraw, char characterOne[TAM]){
+	printf("=========================================================\n");
+	printf("Player01: %c   Vida: %d classe: %c \n", playerOne, lifeOne, characterOne);
+	printf("INIMIGO: Crawmerax       Vida: %d \n", lifeCraw);
+	printf("=========================================================\n");
+}
+//int barbaro(int life, int cure, int offense, int meleeatack);
+void cabecClass(void){
+	printf("==================ESCOLHA SUA CLASSE:====================\n");
+}
+	
+char *classCharacter(int classe){
+	char *characOne;
+	characOne = malloc(TAM*sizeof(char));
 	switch(classe){
-		case 03:break;
-		case 04:break;
-		case 05:break;
+		case 3:
+			strcpy(characOne, "BARBARO"); 
+			break;
+		case 4:
+			strcpy(characOne, "ARQUEIRO"); 
+			break;
+		case 5:
+			strcpy(characOne, "Mago"); 
+			break;
 		default:
 			printf("Opcao invalida! \n");
 	}
+	return characOne;
 }
 
 int main(void){
-	int op;
+	int op, cl;
 	char playerOne[TAM], playerTwo[TAM];
-
+	char cONE[TAM];
 	printf("=========================================================\n");
-	printf("        BEM VINDO AO FOUL BOGEY - Orc Detestavel         \n");
+	printf("        BEM VINDO AO LOP - League of Pobres              \n");
 	printf("================SELECIONE UMA DAS OPCOES=================\n");
 	printf("01 - Single Player\n");
 	printf("02 - Multi Player");
@@ -56,19 +56,30 @@ int main(void){
 	scanf("%d", &op);
 	switch(op){
 		case 01:
-			printf("player 01 - Digite seu nome: \n"); 
-			fgets(playerTwo, TAM, stdin);
-			singlePlayer(playerOne, 100, 100);
+			printf("player 01 - Digite seu nome:"); 
+			scanf("%s",playerOne);
+			cabecClass();
+			printf("03 - Barbaro \n");
+			printf("04 - Arqueiro");
+			printf("\n05 - Mago ");
+			printf("\nEscolha: ");
+			scanf("%d", &cl);		
+			strcpy(cONE, classCharacter(cl));
+			printf("%s : Boa escolha!\n", cONE);
 			break;
+		/*
 		case 02: 
 			printf("Player 01 - Digite seu nome: ");
 			fgets(playerOne, TAM, stdin);
+			printf("Player 02 - Digite seu nome: ");
 			multiPlayers(playerOne, playerTwo, 100, 100, 100);
-			
 			break;
+		*/
 		default:
 			printf("Opcao invalida \n");
-		}	
-	classCharacter();
+		}
+	
+	//cONE = classCharacter();
+	//singlePlayer(playerOne, 100, 100, cONE);
 	return 0;
 }
